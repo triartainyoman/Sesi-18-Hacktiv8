c401abc6a319de6e661c61de97597fc1a863498d
triartainyoman <triarta.art@gmail.com>
2022-03-17 21:18:48 +0800

    Sesi 18

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.img}
+        source={{
+          uri: "https://lampungkode.id/wp-content/uploads/2021/08/react-native-logo.png",
+        }}
+      />
+      <Text style={styles.txt}>Mari Kita Belajar React Native!</Text>
+      <Button title="Click me" color="blue" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  txt: {
+    fontSize: 20,
+  },
+  img: {
+    width: 100,
+    height: 100,
   },
 });
